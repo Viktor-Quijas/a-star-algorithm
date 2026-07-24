ad1f84098473a26710b12e0fbb54c43c541e17cc
Viktor-Quijas <victor.quijas2977@alumnos.udg.mx>
2026-07-24 17:11:46 -0600

    Se añadió la cosa de algorithm

---
+++ src/edu/algoritmo_astar/algorithm/Algorithm.java
@@ -0,0 +1,15 @@
+package edu.algoritmo_astar.algorithm;
+
+public class Algorithm {
+}
+
+
+/*      NOTAS:
+* - Los nodos necesitan saber su valor f, g, h.
+*       - h al ser una operacion se definirá como un metodo y no como un atributo.
+*       - g, aún no sé como determinar su valor. Aunque supongo que al estar en una cuadrícula y su movimiento es en
+*         cruz, sus valores son iguales y la diferencia radica en h. Y creo que readica más en si la casilla está en
+*         blanco o si está ocupada.
+*       - creo que para este caso bidimencinal no necesito generar un grafo, unicamente nos podemos guiar de la matriz.
+*
+* */
